e27631aa28f39d97b988ab56bf46df744937d2e4
Locon213 <167447571+Locon213@users.noreply.github.com>
2025-08-15 02:18:32 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "9.0.0-alpha12" apply false
-    id("com.android.library") version "9.0.0-alpha12" apply false
+    id("com.android.application") version "9.0.0-alpha16" apply false
+    id("com.android.library") version "9.0.0-alpha16" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 }
 
